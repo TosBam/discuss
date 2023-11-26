06729233036ee70dae4e64019d836824ea3782d3
TosBam <samuelson.os19@gmail.com>
2023-11-27 00:05:23 +0100

    Work in progress

--- myapp/discuss.py
+++ myapp/discuss.py
@@ -1,12 +1,21 @@
+#!/usr/bin/python3
 
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/Home")
+def front_page():
+    return render_template('home.html')
+
+@app.route("/About")
+def about_page():
+    return render_template('about.html')
+
+@app.route("/Services")
+def our_service():
+    return render_template('service.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
